872c3a0ef60aa8d944476ff0d156a77f04b3c18d
Azzy-11 <azu11.mermona@gmail.com>
2024-05-26 22:32:05 +0900

    original

--- JSDOM教材/accordion/accordion.js
+++ JSDOM教材/accordion/accordion.js
@@ -1,5 +1,25 @@
 'use strict'
 
 {
+  const menuContents = document.querySelectorAll(".menu-content");
+  const downArrows = document.querySelectorAll(".fa-angle-down");
+  const upArrows = document.querySelectorAll(".fa-angle-up");
 
+  // アコーディオンの内容を表示させる処理
+  downArrows.forEach((downArrow, index) => {
+    downArrow.addEventListener('click', () => {
+      menuContents[index].classList.remove('display-none');
+      upArrows[index].classList.remove('display-none');
+      downArrows[index].classList.add('display-none');
+    })
+  })
+
+  // アコーディオンの内容を非表示させる処理
+  upArrows.forEach((upArrow, index) => {
+    upArrow.addEventListener('click', () => {
+      menuContents[index].classList.add('display-none');
+      upArrows[index].classList.add('display-none');
+      downArrows[index].classList.remove('display-none');
+    })
+  })
 }
