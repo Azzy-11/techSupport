5d9338f71eae1af8b94bd31f516c0232b895be4b
Azzy-11 <azu11.mermona@gmail.com>
2024-05-26 22:38:28 +0900

    toggle

--- JSDOM教材/accordion/accordion.js
+++ JSDOM教材/accordion/accordion.js
@@ -5,6 +5,7 @@
   const downArrows = document.querySelectorAll(".fa-angle-down");
   const upArrows = document.querySelectorAll(".fa-angle-up");
 
+  /*
   // アコーディオンの内容を表示させる処理
   downArrows.forEach((downArrow, index) => {
     downArrow.addEventListener('click', () => {
@@ -21,5 +22,27 @@
       upArrows[index].classList.add('display-none');
       downArrows[index].classList.remove('display-none');
     })
+  }) */
+
+  const toggleAccordion = (index) => {
+    menuContents[index].classList.toggle('display-none');
+    upArrows[index].classList.toggle('display-none');
+    downArrows[index].classList.toggle('display-none');
+  };
+
+  downArrows.forEach((downArrow, index) => {
+    downArrow.addEventListener('click', () => {
+      toggleAccordion(index);
+    })
   })
+
+  upArrows.forEach((upArrow, index) => {
+    upArrow.addEventListener('click', () => {
+      toggleAccordion(index);
+    })
+  })
+
+
+
+  
 }
