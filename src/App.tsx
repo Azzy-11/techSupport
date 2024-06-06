7f0057f9d72653a1ee168488d3aea22e207c606a
Azzy-11 <azu11.mermona@gmail.com>
2024-06-06 23:01:57 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [status, setStatus] = useState("未着手");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>
+          <span>{title}</span>
+          <button>{status}</button>
+          <button>編集</button>
+          <button>削除</button>
+        </li>
+      </ul>      
     </div>
   );
 }
