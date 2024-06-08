c2fbceaf1f107af3e0f2d0c6605e3b727582310c
Azzy-11 <azu11.mermona@gmail.com>
2024-06-09 00:23:05 +0900

    Todo作成と削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,52 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
+  const [id, setId] = useState(1);
   const [title, setTitle] = useState("");
   const [status, setStatus] = useState("未着手");
+  const [list, setList] = useState<any[]>([]);
+
+
+  const handleCreateForm = (e:any) => {
+    setTitle(e.target.value);
+  };
+
+  const handleStoreTodo = () => {
+    setList([
+      ...list,
+    {id: id, title: title, status: status}])
+    setId(id + 1);
+    setTitle("");
+  };
+
+  const handleDeleteTodo = (targetTodo:any) => {
+    setList(list.filter((todo) => todo !== targetTodo));
+  };
 
   return (
-    <div>
+    <React.Fragment>
+      <div>
+        <input 
+        type="text"
+        value={title}
+        onChange={handleCreateForm}
+        />
+        <button onClick={handleStoreTodo}>追加</button>
+      </div>
       <ul>
-        <li>
-          <span>{title}</span>
-          <button>{status}</button>
-          <button>編集</button>
-          <button>削除</button>
-        </li>
+        {list.map((todo) => {
+          return(
+            <li key={todo.id}>
+              <span>{todo.title}</span>
+              <button>{todo.status}</button>
+              <button>編集</button>
+              <button onClick={() => handleDeleteTodo(todo)}>削除</button>
+            </li>
+          )
+        })}
       </ul>      
-    </div>
+    </React.Fragment>
   );
 }
 
